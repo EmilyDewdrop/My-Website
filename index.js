e52075eea899c05e2151bcea8b53c1e5c7cfe9dc
EmilyDewdrop <turner.ek@gmail.com>
2023-03-12 19:34:17 +0000

    All buttons working, pre-mobile optimisation

--- index.js
+++ index.js
@@ -1,23 +1,63 @@
-function clickMeEvent(obj) {
-   if (obj.innerHTML == "Click here to learn more about me!") {obj.innerHTML = "I grew up on the Canadian Prairies";
-   return;
+
+
+document.getElementById("canada").addEventListener("click", explainTextCan);
+
+function explainTextCan() {
+    document.getElementById("infoTest").innerHTML = "I was born and raised near Edmonton, Alberta, Canada, where for a good chunk of the year there is snow as far as you can see. I moved to the UK in 2011, and recently became a British Citizen.";
   }
-  if (obj.innerHTML == "I grew up on the Canadian Prairies") {obj.innerHTML = "I was diagnoised with autism in 2019";
-  return;
- }
- if (obj.innerHTML == "I was diagnoised with autism in 2019") {obj.innerHTML = "I worked in the Houses of Parliament";
- return;
-}
-if (obj.innerHTML == "I worked in the Houses of Parliament") {obj.innerHTML = "I went viral for riding buses in February!";
-return;
-}
-if (obj.innerHTML == "I went viral for riding buses in February!") {obj.innerHTML = "I really want to create tech that makes the world better";
-return;
-}
-if (obj.innerHTML == "I really want to create tech that makes the world better") {obj.innerHTML = "Alight now and explore more!";
-return;
-}
-if (obj.innerHTML == "Alight now and explore more!") {obj.innerHTML = "Click here to learn more about me!";
-return;
-}
-}
+
+document.getElementById("autism").addEventListener("click", explainTextaut);
+
+function explainTextaut() {
+    document.getElementById("infoTest").innerHTML = "In December 2019, I went through the formal process of getting a diagnosis of autism (after having my suspicions for year). Since then I've completely embraced being autistic, and started to really explore my autistic tendancies. One of these is my tendancy to hyperfocus and stim when I'm doing something I really enjoy, and I've noticed I do a lot of both of those when coding.";
+  }
+
+  document.getElementById("parliament").addEventListener("click", explainTextPar);
+
+  function explainTextPar() {
+      document.getElementById("infoTest").innerHTML = "One of my favourite ever jobs was with the Education department at The Houses of Parliament. Every day for two years I would take school groups from around the country on tours and introduce them to how Parliamentary democracy worked. Though I left for a myriad of reasons, including low pay and feeling restricted by online impartiality requirements (something I've been reflecting on with the recently BBC debates), I considered the role a great honour.";
+    }
+
+    document.getElementById("busTrip").addEventListener("click", explainTextBus);
+
+    function explainTextBus() {
+        document.getElementById("infoTest").innerHTML = "For my birthday in February, I decided to take advantage of the teporary £2 fare cap on buses in England and achieve a long-time goal of making a journey from London to Edinburgh via local buses. When I sent out a tweet about this to my 800 followers, I wasn't expecting much of a response, but within 24 hours I had gained 20,000 new followers. I appeared in national and international media, have appeared at Bus Industry events, and even got a literary agent out of the experience!";
+    }
+      
+    document.getElementById("betterWorld").addEventListener("click", explainTextBetter);
+
+    function explainTextBetter() {
+        document.getElementById("infoTest").innerHTML = "Saying I want to create tech for a better world sounds cliche, but I recently took part in Freedom From Torture's Campiagn Leadership Programme and the group had so many great ideas, but a lot of them required technical skills I just didn't have. I also work for an advocacy charity, and not-fit-for-purpose technology really holds back the work advocates do. I often feel helpless in the face of so many major crises in the world, and I'm inspired by apps like Olio or StreetLink that make a tangible difference.";
+    }
+      
+    document.getElementById("learnMore").addEventListener("click", explainTextMore);
+
+    function explainTextMore() {
+        document.getElementById("infoTest").innerHTML = "Scroll down to see the gallery I created to highlight the other projects in my Founders and Coders application.";
+    }
+
+
+
+    function clickMeEvent(obj) {
+        if (obj.innerHTML == "Click here to learn more about me!") {obj.innerHTML = "I grew up on the Canadian Prairies";
+        return;
+       }
+       if (obj.innerHTML == "I grew up on the Canadian Prairies") {obj.innerHTML = "I was diagnoised with autism in 2019";
+       return;
+      }
+      if (obj.innerHTML == "I was diagnoised with autism in 2019") {obj.innerHTML = "I worked in the Houses of Parliament";
+      return;
+     }
+     if (obj.innerHTML == "I worked in the Houses of Parliament") {obj.innerHTML = "I went viral for riding buses in February!";
+     return;
+     }
+     if (obj.innerHTML == "I went viral for riding buses in February!") {obj.innerHTML = "I really want to create tech that makes the world better";
+     return;
+     }
+     if (obj.innerHTML == "I really want to create tech that makes the world better") {obj.innerHTML = "Alight now and explore more!";
+     return;
+     }
+     if (obj.innerHTML == "Alight now and explore more!") {obj.innerHTML = "Click here to learn more about me!";
+     return;
+     }
+     }
