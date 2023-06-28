265153e0f44ec7734b4c1da567ecaf6239f2a586
EmilyDewdrop <turner.ek@gmail.com>
2023-06-28 20:10:50 +0100

    imported gallery

--- index.js
+++ index.js
@@ -61,3 +61,39 @@ function explainTextaut() {
      return;
      }
      }
+
+     function toggleText1() {
+        var text = document.getElementById("hobbyText");
+        if (text.style.display === "none") {
+          text.style.display = "block";
+        } else {
+          text.style.display = "none";
+        }
+      }
+    
+      function toggleText2() {
+        var text = document.getElementById("movieText");
+        if (text.style.display === "none") {
+          text.style.display = "block";
+        } else {
+          text.style.display = "none";
+        }
+      }
+    
+      function toggleText3() {
+        var text = document.getElementById("websiteText");
+        if (text.style.display === "none") {
+          text.style.display = "block";
+        } else {
+          text.style.display = "none";
+        }
+      }
+    
+      function toggleText4() {
+        var text = document.getElementById("gameText");
+        if (text.style.display === "none") {
+          text.style.display = "block";
+        } else {
+          text.style.display = "none";
+        }
+      }
